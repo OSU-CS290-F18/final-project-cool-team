8b381c64bb4bdbf6bfdfe78277b7d05f2e7a7c0a
Steven <nguyens5@oregonstate.edu>
2018-11-29 23:05:26 -0800

    my stuff plus logo

--- Public/index.js
+++ Public/index.js
@@ -1,1 +1,17 @@
 // js file
+function filterFunction() {
+    document.getElementById("myfilter").classList.toggle("show");
+}
+window.onclick = function(event) {
+  if (!event.target.matches('.filter-button')) {
+
+    var filters = document.getElementsByClassName("filter-content");
+    var i;
+    for (i = 0; i < filters.length; i++) {
+      var openfilter = filters[i];
+      if (openfilter.classList.contains('show')) {
+        openfilter.classList.remove('show');
+      }
+    }
+  }
+}
