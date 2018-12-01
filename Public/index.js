e7db77a035ea349cf59454dea36d8cf32d4d0cff
Matthew Levis <matthewlev13@comcast.net>
2018-11-30 16:10:15 -0800

    added stevens index.js to master

--- Public/index.js
+++ Public/index.js
@@ -8,3 +8,20 @@ function show_newpost(){
 	document.getElementById('newpost-container').style.visibility = 'visible';
 	document.getElementById('newpost-container').style.display = 'block';
 }
+
+function filterFunction() {
+    document.getElementById("myfilter").classList.toggle("show");
+}
+window.onclick = function(event) {
+  if (!event.target.matches('.filter-button')) {
+
+    var filters = document.getElementsByClassName("filter-content");
+    var i;
+    for (i = 0; i < filters.length; i++) {
+      var openfilter = filters[i];
+      if (openfilter.classList.contains('show')) {
+        openfilter.classList.remove('show');
+      }
+    }
+  }
+}
