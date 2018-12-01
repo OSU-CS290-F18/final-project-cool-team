c9931900ec7a8a0453cbe4fbaa5205ea0698c850
Matthew Levis <matthewlev13@comcast.net>
2018-11-30 16:01:32 -0800

    added changes to server.js and index.html

--- Public/index.js
+++ Public/index.js
@@ -1,1 +1,10 @@
 // js file
+
+
+document.getElementById('newpost-button').onclick = function(){show_newpost()};
+
+function show_newpost(){
+	document.getElementById('newpost-button').style.visibility = 'hidden';
+	document.getElementById('newpost-container').style.visibility = 'visible';
+	document.getElementById('newpost-container').style.display = 'block';
+}
