1981f0b4dda22c0dc2d89c3bad77057c8f5562be
camman <c.grein123@gmail.com>
2018-12-02 13:44:39 -0800

    added another button interaction

--- Public/index.js
+++ Public/index.js
@@ -3,6 +3,8 @@
 
 document.getElementById('newpost-button').onclick = function(){show_newpost()};
 document.getElementById('cancel-button').onclick = function(){hide_newpost()};
+document.getElementById('add-tag-button').onclick = function(){show_tag_maker()};
+document.getElementById('cancel-newtag-button').onclick = function(){hide_tag_maker()};
 
 function show_newpost(){
 	document.getElementById('newpost-button').style.visibility = 'hidden';
@@ -13,7 +15,21 @@ function hide_newpost(){
 	document.getElementById('newpost-button').style.visibility = 'visible';
 	document.getElementById('newpost-container').style.visibility = 'hidden';
 	document.getElementById('newpost-button').style.display = 'block';
+	document.getElementById('post-button').style.visibility = 'hidden';
+	document.getElementById('tag-add').style.visibility = 'hidden';
 }
+function show_tag_maker(){
+	document.getElementById('tag-add').style.visibility = 'visible';
+	document.getElementById('tag-add').style.display = 'block';
+	document.getElementById('post-button').style.visibility = 'hidden';
+}
+function hide_tag_maker(){
+	document.getElementById('post-button').style.visibility = 'visible';
+	document.getElementById('post-button').style.display = 'block';
+	document.getElementById('tag-add').style.visibility = 'hidden';
+}
+
+
 function filterFunction() {
     document.getElementById("myfilter").classList.toggle("show");
 }
