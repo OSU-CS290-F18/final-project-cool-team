3742bb040486f499914fea5a2f6d0233808a086a
camman12 <43551886+camman12@users.noreply.github.com>
2018-12-03 11:09:12 +0900

    yee

--- server.js
+++ server.js
@@ -2,9 +2,7 @@ var path = require('path');
 var express = require('express');
 var exphbs = require('express-handlebars');
 var app = express();
-
-
-
+postData = require('./postData');
 var port= process.env.PORT || 3000;
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
@@ -13,7 +11,10 @@ app.set('view engine', 'handlebars');
 app.use(express.static('public'));
 
 app.get('/', function(req, res, next) {
-    res.status(200).render('home'); 
+    res.status(200).render('home',{
+		posts: postData;
+		
+	}); 
 });
 
 app.get('*', function (req, res, next) {
