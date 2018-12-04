ace3c60c38b22200c6fb625f030449027a73f5a0
SamHaup <samhaup@gmail.com>
2018-12-04 00:12:13 -0800

    Server

--- Public/index.js
+++ Public/index.js
@@ -1,19 +1,38 @@
-// js file
 
+
+var allPosts = [];
 
 document.getElementById('newpost-button').onclick = function(){show_newpost()};
 document.getElementById('cancel-button').onclick = function(){hide_newpost()};
-
+document.getElementById('add-tag-button').onclick = function(){show_tag_maker()};
+document.getElementById('cancel-newtag-button').onclick = function(){hide_tag_maker()};
+document.getElementById('filter-update-button').onclick = function(){Search()};
 function show_newpost(){
 	document.getElementById('newpost-button').style.visibility = 'hidden';
 	document.getElementById('newpost-container').style.visibility = 'visible';
 	document.getElementById('newpost-container').style.display = 'block';
+	document.getElementById('post-button').style.visibility = 'visible';
+	document.getElementById('post-button').style.display = 'block';
 }
 function hide_newpost(){
 	document.getElementById('newpost-button').style.visibility = 'visible';
 	document.getElementById('newpost-container').style.visibility = 'hidden';
 	document.getElementById('newpost-button').style.display = 'block';
+	document.getElementById('post-button').style.visibility = 'hidden';
+	document.getElementById('tag-add').style.visibility = 'hidden';
 }
+function show_tag_maker(){
+	document.getElementById('tag-add').style.visibility = 'visible';
+	document.getElementById('tag-add').style.display = 'block';
+	document.getElementById('post-button').style.visibility = 'hidden';
+}
+function hide_tag_maker(){
+	document.getElementById('post-button').style.visibility = 'visible';
+	document.getElementById('post-button').style.display = 'block';
+	document.getElementById('tag-add').style.visibility = 'hidden';
+}
+
+
 function filterFunction() {
     document.getElementById("myfilter").classList.toggle("show");
 }
@@ -29,4 +48,151 @@ window.onclick = function(event) {
       }
     }
   }
+}
+
+function insertNewFoto(id, imgURL, description, tags, date, comments) {
+	var postHTML = Handlebars.templates.foto({
+		id: id,
+		imgURL: imgURL,
+		description: description,
+		tags: tags,
+		date: date,
+		comments: comments
+	});
+	var postContainer = document.getElementById('images');
+	postContainer.insertAdjacentHTML('beforeend', postHTML);
+    console.log(postHTML);
+    var postRequest = new XMLHttpRequest();
+    var requestURL = '/foto' + postHTML.id + '/addFoto';
+    postRequest.open('POST', requestURL);
+    var requestBODY = JSON.stringify({
+        id: id,
+		imgURL: imgURL,
+		description: description,
+		tags: tags,
+		date: date,
+		comments: comments
+     });
+    postRequest.addEventListener('load', function(event) {
+        if(event.target.status==200) {
+           var postHTML= Handlebars.templates.foto({
+            id: id,
+		    imgURL: imgURL,
+		    description: description,
+		    tags: tags,
+		    date: date,
+		    comments: comments
+	    });
+        var photoCardContainer = document.querySelector('.photo-card-container')
+        photoCardContainer.insertAdjacentHTML('beforeend', postHTML);
+
+
+
+    console.log(requestBODY);
+}
+
+// function insertTags(tags){
+//
+// 	var tagHTML = Handlebars.templates.tag({
+// 		tags: tags
+// 	});
+// 	var tagsContainer = document.getElementById('tags');
+// 	tagsContainer.insertAdjacentHTML('beforeend', tagHTML);
+//
+// }
+
+var currentDate = new Date();
+var day = currentDate.getDate();
+var month = currentDate.getMonth() + 1;
+var year = currentDate.getFullYear();
+
+
+var tagsArray = [];
+// var checkboxes = document.querySelectorAll('input[type=checkbox]:checked');
+//
+// for (var i = 0; i < checkboxes.length; i++) {
+// 	if (checkboxes[i].type == 'checkbox' && checkboxes[i].checked == true) {
+// 		console.log("====----" + checkboxes[i]);
+// 		tagsArray.push(checkboxes[i].name);
+// 	}
+// }
+console.log("=====" + tagsArray);
+
+var description = document.getElementById('newpost-text-imgURL');
+var imgURL = document.getElementById('newpost-text-name');
+var date = "" + month + "/" + day + "/" + year;
+var comments = [];
+
+
+var newPost = document.getElementById('post-button');
+newPost.addEventListener('click', function() {
+	var checkboxes = document.querySelectorAll('input[type=checkbox]:checked');
+
+	for (var i = 0; i < checkboxes.length; i++) {
+		if (checkboxes[i].type == 'checkbox' && checkboxes[i].checked == true) {
+			console.log("====----" + checkboxes[i]);
+			tagsArray.push(checkboxes[i].name);
+		}
+	}
+	if (description && description.value && imgURL && imgURL.value) {
+		insertNewFoto(
+			3 + allPosts.length,
+			document.getElementById('newpost-text-imgURL').value,
+			document.getElementById('newpost-text-name').value,
+			tagsArray,
+			"" + month + "/" + day + "/" + year,
+			[]
+		);
+		document.getElementById('newpost-text-name').value = "";
+		document.getElementById('newpost-text-imgURL').value = "";
+		tagsArray = [];
+		for (var i = 0; i < checkboxes.length; i++) {
+			checkboxes[i].checked = false;
+		}
+		//uncheck tags
+
+	} else {
+		alert("You must enter the description and url fields to make a post");
+	}
+
+	allPosts.push({
+		id: 3 + allPosts.length,
+		description: description,
+		imgURL: imgURL,
+		tags: tagsArray,
+		date: date,
+		comments: comments
+	});
+
+});
+
+var commentContainer = document.getElementById('image-comments');
+var commentInput = document.getElementById('add-comment-input');
+var commentButton = document.getElementById('comment-button');
+commentButton.addEventListener('click', function() {
+	if (commentInput && commentInput.value) {
+		commentContainer.insertAdjacentHTML('beforeend', '<div class="image-comment"><i>"' + commentInput.value + '"</i></div>');
+		document.getElementById('add-comment-input').value = "";
+	} else {
+		alert("You must enter a comment before adding one!")
+	}
+});
+
+function Search(){ 
+	var images = document.getElementById('images').children; 
+	for(var i = 0; i < images.length; i++){
+		var test = true; 
+		var title = images[i].getElementById('image-title').innerHTML.toUpperCase();
+		console.log(title);
+		if(document.getElementById('filter-text') != ''){
+			if(document.getElementById('filter-city').value.toUpperCase() !== title.toUpperCase()){
+				test = false; 
+		
+			}
+		}
+	
+		if(test = false){
+			posts[i].display = 'none';
+		}
+	}
 }
--- server.js
+++ server.js
@@ -2,20 +2,73 @@ var path = require('path');
 var express = require('express');
 var exphbs = require('express-handlebars');
 var app = express();
-
-
-
+var MongoClient = require('mongodb').MongoClient; 
+var bodyParser = require('body-parser'); 
+postData = require('./postData');
 var port= process.env.PORT || 3000;
+var url = "mongodb://sam:6a32Bc91@foto-shard-00-00-cjfic.mongodb.net:27017,foto-shard-00-01-cjfic.mongodb.net:27017,foto-shard-00-02-cjfic.mongodb.net:27017/test?ssl=true&replicaSet=foto-shard-0&authSource=admin&retryWrites=true"
+var db; 
+var photos; 
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
+app.use(bodyParser.json());
+
 app.use(express.static('public'));
 
+
+/*app.get('/', function(req, res, next) {
+	res.status(200).render('home',{
+		posts: postData,
+    tags: postData[0].tags
+	});
+});*/
+
 app.get('/', function(req, res, next) {
-    res.status(200).render('home', OURPAGEDATA);
-    console.log("==home");
+	 photos.find({}).toArray(function (err, docs) {
+        console.log("posts------------------------------ docs")
+        console.log(docs);
+		if (err) {
+		res.status(500).send("cannot connect to DB.");
+		}
+    var arrayOfTags = [];
+    for(var i = 0; i < docs.length; i++) {
+        for(var j=0; j < docs[i].tags.length; j++) {
+            arrayOfTags.push(docs[i].tags[j]);
+        }
+
+    }
+    
+
+	console.log(docs);
+    res.status(200).render('home', {
+      posts: docs
+      tags: arrayOfTags
+    });
+    tags: postData[0].tags
+	});
 });
+
+app.get('/foto:id', function(req, res, next) {
+    var id = req.params.id;
+    var index = findWithAttr(postData, 'id', id);
+    if (postData[id]) {
+      res.status(200).render('partials/foto', {
+        id: postData[index].id,
+        description: postData[index].description,
+        imgURL: postData[index].imgURL,
+        tags: postData[index].tags,
+        comments: postData[index].comments,
+        date: postData[index].date
+  	  });
+
+    } else {
+      next();
+    }
+});
+
+//app.post(/
 
 app.get('*', function (req, res, next) {
   res.status(404).render('404');
@@ -28,6 +81,23 @@ app.listen(port, function (err) {
   console.log("== Server is listening on port", port);
 });
 
+function findWithAttr(jsonArray, attr, value) {
+  for (var i = 0; i < postData.length; i++) {
+    if (postData[i][attr] == value) {
+      return i
+    }
+  }
+  return -1;
+}
 
-
-
+MongoClient.connect(url, function(err, client){
+	if(err){
+		throw err; 
+	}
+	console.log("mongodb is connected");
+	db = client.db('test');
+	photos = db.collection('foto'); 
+	photos.find().toArray(function(err, docs){
+		console.log(docs) 
+	});
+});
